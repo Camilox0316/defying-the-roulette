d3a8a9273a0e65ffba9f4e14e684a0090b2e7947
Camilox0316 <camsanchezr03@gmail.com>
2024-06-27 19:55:37 -0600

    Martingala finished

--- roulette.sh
+++ roulette.sh
@@ -36,49 +36,55 @@ allowedTechniques(){
 
 function martinGala(){
 
-  echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Current money:${endColour} ${blueColour}$money\$${endColour}"
+  #echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Current money:${endColour} ${blueColour}$money\$${endColour}"
   echo -ne "${yellowColour}[+]${endColour} ${grayColour}How much money do you want to bet? -> ${endColour}" && read initial_bet
   echo -ne "${yellowColour}[+]${endColour} ${grayColour}What do you want to bet continuosly (even/odd)? -> ${endColour}" && read odd_even
-  
-  echo -e "\n${yellowColour}[+]${endColour} ${grayColour}We are going to play with${endColour}${blueColour} ${initial_bet}\$ ${endColour}${grayColour}to${endColour}${purpleColour} ${odd_even}${endColour}\n"
+  #echo -e "\n${yellowColour}[+]${endColour} ${grayColour}We are going to play with${endColour}${blueColour} ${initial_bet}\$ ${endColour}${grayColour}to${endColour}${purpleColour} ${odd_even}${endColour}\n"
   tput civis
   backup_bet=$initial_bet
-
+  turns_counter=1
+  bad_turns="[ "
+  max_prize=$initial_bet
   while [ ! $money -le 0 ]; do
     
     random_number="$(($RANDOM % 37))"
     money=$(($money-$initial_bet))
-    echo -e "\n${yellowColour}[+]${endColour} ${grayColour}You bet${endColour}${blueColour} ${initial_bet}\$ ${endColour} ${grayColour}and you have${endColour} ${blueColour}${money}\$ ${endColour}"
-    echo -e "${yellowColour}[+]${endColour} ${grayColour}Random number: ${endColour}${yellowColour}$random_number${endColour}"
-
-    #if [[ "$random_number" -eq 0 ]]; then
-    #  echo -e "${yellowColour}[+]${endColour} ${grayColour}Number is 0, you lost!${endColour}"
-    
-    #if { [[ "$random_number" -eq 0 ]] } && { { [ "$(($random_number % 2))" == "0" ] && [ "$odd_even" == "even" ]; } || { [ "$(($random_number % 2))" == "1" ] && [ "$odd_even" == "odd" ]; }; }; then
+    #echo -e "\n${yellowColour}[+]${endColour} ${grayColour}You bet${endColour}${blueColour} ${initial_bet}\$ ${endColour} ${grayColour}and you have${endColour} ${blueColour}${money}\$ ${endColour}"
+    #echo -e "${yellowColour}[+]${endColour} ${grayColour}Random number: ${endColour}${yellowColour}$random_number${endColour}"
 
     if [[ "$random_number" -ne 0 ]] && { { [[ "$(($random_number % 2))" -eq 0 ]] && [[ "$odd_even" == "even" ]]; } || { [[ "$(($random_number % 2))" -eq 1 ]] && [[ "$odd_even" == "odd" ]]; }; }; then
-    #elif { [ "$(($random_number % 2))" == "0" ] && [ "$odd_even" == "even" ]; } || { [ "$(($random_number % 2))" == "1" ] && [ "$odd_even" == "odd" ]; }; then
-      echo -e "${yellowColour}[+]${endColour} ${greenColour}You win! :D${endColour}"
+      #echo -e "${yellowColour}[+]${endColour} ${greenColour}You win! :D${endColour}"
       reward=$(($initial_bet*2))    
-      echo -e "${yellowColour}[+]${endColour} ${grayColour}You won: ${endColour} ${blueColour}$reward\$ ${endColour}"
+      #echo -e "${yellowColour}[+]${endColour} ${grayColour}You won: ${endColour} ${blueColour}$reward\$ ${endColour}"
       money=$(($money+$reward)) 
-      echo -e "${yellowColour}[+]${endColour} ${grayColour}Now you have: ${endColour}${blueColour}$money\$ ${endColour}"
+      #echo -e "${yellowColour}[+]${endColour} ${grayColour}Now you have: ${endColour}${blueColour}$money\$ ${endColour}"
       initial_bet=$backup_bet
+      bad_turns=""
+      max_prize=$money
     else
-      echo -e "${yellowColour}[+]${endColour} ${redColour}You lose! :(${endColour}"
+      #echo -e "${yellowColour}[+]${endColour} ${redColour}You lose! :(${endColour}"
       initial_bet=$(($initial_bet*2))
       money=$(($money))
-      echo -e "${yellowColour}[+]${endColour} ${grayColour}Now you have: ${endColour}${blueColour}$money\$ ${endColour}"
+      #echo -e "${yellowColour}[+]${endColour} ${grayColour}Now you have: ${endColour}${blueColour}$money\$ ${endColour}"
+      bad_turns+="$random_number "
       if [ $money -le 0 ]; then
+        #echo -e "\n${redColour}[!] You ran out of money${endColour}\n"
         echo -e "\n${redColour}[!] You ran out of money${endColour}\n"
-        exit 0
+        echo -e "\n${yellowColour}[+]${endColour}${grayColour} Total turns:${endColour} ${purpleColour}$turns_counter${endColour}"
+        echo -e "\n${yellowColour}[+]${endColour} ${grayColour} The max prize was: ${endColour}${greenColour}$max_prize${endColour}"
+        echo -e "\n${yellowColour}[+]${endColour}${grayColour} Bad turns:${endColour} ${redColour}$bad_turns${endColour}\n"
+
+        tput cnorm && exit 0
       fi
     fi
-    sleep 0.4
+
+    let turns_counter+=1
+    #sleep 0.4
+
   done
   tput cnorm
-  echo -e "\n${redColour}[!] You ran out of money${endColour}\n"
   exit 0
+
 }
 
 inverseLabrouchere(){
